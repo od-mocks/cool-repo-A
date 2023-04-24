0d0f5d8383f70f71bb6d21f1b2a2248e4d23f2be
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2023-04-24 10:35:57 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -10,5 +10,5 @@ fn main() {
     // Statements here are executed when the compiled binary is called
 
     // Print text to the console
-    println!("Hello World!");
+    println!("Hello World at 24-Apr-2023!");
 }
